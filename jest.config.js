65bfb8ae56ef896d15e40ddfd77b943e7bd3f0b2
mechanik-daniel <daniel@outburn.co.il>
2025-12-09 19:54:45 +0200

    test: update jest config to ignore integration tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
+/* eslint-env node */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/integration/'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
 };
