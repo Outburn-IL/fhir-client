0425a1ace37889a889d844cec6b2743bd8d7287b
mechanik-daniel <daniel@outburn.co.il>
2026-02-08 13:39:40 +0200

    refactor: add ESLint configuration and upgrade eslint dependencies

---
+++ eslint.config.cjs
@@ -0,0 +1,24 @@
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = [
+  {
+    ignores: ['dist/**', 'coverage/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+];
